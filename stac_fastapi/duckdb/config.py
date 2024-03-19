0ab0b12edc7ad0a32fa8bffbd2872ddbf9d7847d
jonhealy1 <jonathan.d.healy@gmail.com>
2024-03-19 23:58:15 +0800

    fix typo

--- stac_fastapi/duckdb/config.py
+++ stac_fastapi/duckdb/config.py
@@ -5,7 +5,7 @@ from stac_fastapi.types.config import ApiSettings
 
 
 class DuckDBSettings(ApiSettings):
-    """MongoDB specific API settings."""
+    """DuckDB specific API settings."""
 
     forbidden_fields: Set[str] = {"type"}
     indexed_fields: Set[str] = {"datetime"}
